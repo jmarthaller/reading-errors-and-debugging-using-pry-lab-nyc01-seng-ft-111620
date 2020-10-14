fab47721fac103a7d13262ac8fe5d41bf8d2bde8
Jacob Marthaller <marthaller.jr@gmail.com>
2020-10-14 15:31:41 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,7 +3,7 @@ require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
     10.times do 
-      string[0] += 's'
+      string[0] += 's'*10
     # "s" + "s" + "s" + "s" + "s" + "s" + "s" + "s" + "s" + "s" + string
   end
   else
